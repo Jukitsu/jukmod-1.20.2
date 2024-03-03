77d023631c6753f4c06738ef9c7fd949c874ab12
Jukitsu <ninjax2312@gmail.com>
2024-03-03 18:26:05 +0100

    Initial Commit

---
+++ src/client/java/net/jukitsumc/jukmod/JukmodClient.java
@@ -0,0 +1,10 @@
+package net.jukitsumc.jukmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JukmodClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
