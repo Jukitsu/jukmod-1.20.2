1234500930abac878b16c0c000d7b4713e74f8ee
Jukitsu <ninjax2312@gmail.com>
2024-03-29 18:56:50 +0100

    Update MobMixin.java

--- src/main/java/net/jukitsumc/jukmod/mixin/MobMixin.java
+++ src/main/java/net/jukitsumc/jukmod/mixin/MobMixin.java
@@ -21,29 +21,29 @@ public abstract class MobMixin extends LivingEntity {
     @Final
     private BodyRotationControl bodyRotationControl;
 
+    @Shadow public abstract int getMaxHeadYRot();
     protected MobMixin(EntityType<? extends Mob> entityType, Level level) {
         super(entityType, level);
     }
 
     @Overwrite
     public float tickHeadTurn(float f, float g) {
-        double d = this.getX() - this.xo;
-        double e = this.getZ() - this.zo;
-        if (d * d + e * e > 2.500000277905201E-7D) {
-            float h = Mth.wrapDegrees(f - this.yBodyRot);
-            this.yBodyRot += h * 0.3F;
-            float i = Mth.wrapDegrees(this.getYRot() - this.yBodyRot);
-            if (Math.abs(i) > 50.0F) {
-                this.yBodyRot += i - (float) (Mth.sign(i) * 50);
-            }
-
-            boolean bl = i < -90.0F || i >= 90.0F;
-            if (bl) {
-                g *= -1.0F;
-            }
+        float h = Mth.wrapDegrees(f - this.yBodyRot);
+        this.yBodyRot += h * 0.3F;
+        float i = Mth.wrapDegrees(this.yHeadRot - this.yBodyRot);
+        if (Math.abs(i) > this.getMaxHeadYRot()) {
+            this.yBodyRot += i - (float) (Mth.sign(i) * this.getMaxHeadYRot());
         }
 
+        boolean bl = i < -90.0F || i >= 90.0F;
+        if (bl) {
+            g *= -1.0F;
+        }
+
+
         this.bodyRotationControl.clientTick();
+
+
         return g;
     }
 }
