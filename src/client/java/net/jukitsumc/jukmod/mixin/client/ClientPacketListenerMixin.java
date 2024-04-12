be67eeaccb770d9a3d72d4500ecfe0a48615a170
Jukitsu <84381972+Jukitsu@users.noreply.github.com>
2024-04-12 12:52:33 +0200

    E

--- src/client/java/net/jukitsumc/jukmod/mixin/client/ClientPacketListenerMixin.java
+++ src/client/java/net/jukitsumc/jukmod/mixin/client/ClientPacketListenerMixin.java
@@ -1,8 +1,12 @@
 package net.jukitsumc.jukmod.mixin.client;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.client.server.IntegratedServer;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
@@ -10,9 +14,11 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(ClientPacketListener.class)
 public class ClientPacketListenerMixin {
 
-    @Redirect(method = "handleMoveEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;lerpTo(DDDFFI)V"))
-    private void doNotSmoothEntityMovement(Entity entity, double x, double y, double z, float g, float h, int steps) {
+    private final Minecraft minecraft = Minecraft.getInstance();
 
-        entity.lerpTo(x, y, z, g, h, 1);
+    @Redirect(method = "handleMoveEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;lerpTo(DDDFFI)V"))
+    private void properlyLerpEntityMovement(Entity entity, double x, double y, double z, float g, float h, int steps) {
+        int lerpSteps = this.minecraft.isSingleplayer() ? 1 : entity.getType().updateInterval();
+        entity.lerpTo(x, y, z, g, h, lerpSteps);
     }
 }
--- src/main/java/net/jukitsumc/jukmod/mixin/ServerEntityMixin.java
+++ src/main/java/net/jukitsumc/jukmod/mixin/ServerEntityMixin.java
@@ -2,20 +2,26 @@ package net.jukitsumc.jukmod.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.server.level.ServerEntity;
+import net.minecraft.server.level.ServerLevel;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(ServerEntity.class)
 public class ServerEntityMixin {
 
+    @Shadow @Final
+    protected ServerLevel level;
+
 
     @ModifyExpressionValue(method = "sendChanges", at = @At(value = "FIELD", target = "Lnet/minecraft/server/level/ServerEntity;tickCount:I", ordinal = 2))
     private int hijackTickCountSoServerSendEntityUpdateEveryTick(int fuckThisShitImOut) {
-        return 60;
+        return level.getServer().isDedicatedServer() ? fuckThisShitImOut : 60;
     }
 
     @ModifyExpressionValue(method = "sendChanges", at = @At(value = "FIELD", target = "Lnet/minecraft/server/level/ServerEntity;updateInterval:I"))
     private int modifyUpdateInterval(int interval) {
-        return 1;
+        return level.getServer().isDedicatedServer() ? interval : 1;
     }
 }
