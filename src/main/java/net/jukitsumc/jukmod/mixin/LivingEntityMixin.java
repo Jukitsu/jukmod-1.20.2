00b5907f85746223b52610213d69e6014a09fc10
Jukitsu <ninjax2312@gmail.com>
2024-05-11 14:20:58 +0200

    Revert "Kohi Kb"
    This reverts commit 2e390b9febf9b6641fbceda93bba349d7b01892d.

--- src/main/java/net/jukitsumc/jukmod/mixin/LivingEntityMixin.java
+++ src/main/java/net/jukitsumc/jukmod/mixin/LivingEntityMixin.java
@@ -3,15 +3,11 @@ package net.jukitsumc.jukmod.mixin;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.jukitsumc.jukmod.Jukmod;
-import net.jukitsumc.jukmod.KohiConstants;
 import net.jukitsumc.jukmod.config.option.BooleanOption;
-import net.jukitsumc.jukmod.config.option.LongSliderOption;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.attributes.Attribute;
-import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.BooleanOp;
@@ -27,62 +23,25 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
 
+
     @Unique
     private BooleanOption oldPlayerBackwardsOption;
 
     @Unique
     private BooleanOption oldClientMovement;
 
-    @Unique
-    private LongSliderOption knockbackType;
-
     @Inject(method="<init>", at=@At("TAIL"))
     private void initialize(CallbackInfo ci) {
         oldPlayerBackwardsOption = Jukmod.getInstance().getConfig().animations().oldPlayerBackwards();
         oldClientMovement = Jukmod.getInstance().getConfig().entities().oldClientMovement();
-        knockbackType = Jukmod.getInstance().getConfig().gameplay().knockbackType();
     }
 
     protected LivingEntityMixin(EntityType<? extends LivingEntity> entityType, Level level) {
         super(entityType, level);
     }
 
-    @Shadow public abstract double getAttributeValue(Attribute attribute);
-
     @Shadow
     public abstract void knockback(double d, double e, double f);
-
-    @Inject(method="knockback", at=@At("HEAD"), cancellable = true)
-    public void addBackKohiKnockback(double amount, double x, double z, CallbackInfo ci) {
-        if (knockbackType.get() == 0) { // KOHI KNOCKBACK
-            amount *= 1.0D - this.getAttributeValue(Attributes.KNOCKBACK_RESISTANCE);
-            if (amount > 0.0D) {
-                this.hasImpulse = true;
-                Vec3 velocity = this.getDeltaMovement();
-                Vec3 knockbackVector = (new Vec3(x, 0.0D, z)).normalize().scale(KohiConstants.knockbackHorizontal);
-                this.setDeltaMovement(
-                        velocity.x / KohiConstants.knockbackFriction - knockbackVector.x,
-                        Math.min(KohiConstants.knockbackVerticalLimit, velocity.y / 2.0D + KohiConstants.knockbackVertical),
-                        velocity.z / KohiConstants.knockbackFriction - knockbackVector.z
-                );
-            }
-            ci.cancel();
-        } else if (knockbackType.get() == -1) { // 1.8 Knockback
-            amount *= 1.0D - this.getAttributeValue(Attributes.KNOCKBACK_RESISTANCE);
-            if (amount > 0.0D) {
-                this.hasImpulse = true;
-                Vec3 velocity = this.getDeltaMovement();
-                Vec3 knockbackVector = (new Vec3(x, 0.0D, z)).normalize().scale(amount);
-                this.setDeltaMovement(
-                        velocity.x / 2.0D - knockbackVector.x,
-                        Math.min(0.4D, velocity.y / 2.0D + amount),
-                        velocity.z / 2.0D - knockbackVector.z
-                );
-            }
-            ci.cancel();
-        }
-
-    }
 
 
     @ModifyExpressionValue(method="travel", at=@At(value="INVOKE", target="Lnet/minecraft/world/entity/LivingEntity;isControlledByLocalInstance()Z"))
